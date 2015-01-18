07a34d3248ac7d3ebccf6b90f634224c6bd7b3dd
yanchangyou <yanchangyou@gmail.com>
2015-01-18 11:36:27 +0800

    设置小车的gpio编号：右轮 0(17),1(18)，左轮 4（23）,5（24）。 红外线使用25

--- shell/bin/setup.sh
+++ shell/bin/setup.sh
@@ -4,11 +4,10 @@
 
 gpio_path=/usr/local/bin
 
-#right
+#right wheel
 $gpio_path/gpio mode 0 out
 $gpio_path/gpio mode 1 out
 
-#left
+#left wheel
 $gpio_path/gpio mode 4 out
 $gpio_path/gpio mode 5 out
-
