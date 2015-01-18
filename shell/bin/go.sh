eecd50e24134c0655defa12f8ca1ad897941ef60
yanchangyou <yanchangyou@gmail.com>
2015-01-18 11:31:32 +0800

    小车开动方向---传递参数：left,right,forward,backward

--- shell/bin/go.sh
+++ shell/bin/go.sh
@@ -2,10 +2,10 @@
 
 gpio_path=/usr/local/bin
 
-#right
+#right wheel
 $gpio_path/gpio write 0 $1
 $gpio_path/gpio write 1 $2
 
-#left
+#left wheel
 $gpio_path/gpio write 4 $3
 $gpio_path/gpio write 5 $4
